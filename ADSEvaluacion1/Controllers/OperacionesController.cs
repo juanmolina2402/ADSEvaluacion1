9a261228f27675fff09ffa7db40a2b2b50fffa0f
juanmolina2402 <1cajuanmolina@gmail.com>
2021-03-13 16:25:17 -0600

    .

--- ADSEvaluacion1/Controllers/OperacionesController.cs
+++ ADSEvaluacion1/Controllers/OperacionesController.cs
@@ -10,13 +10,33 @@ namespace ADSEvaluacion1.Controllers
     public class OperacionesController : Controller
     {
         // GET: Operaciones
-        public ActionResult Operaciones_Aritmeticas()
+        public ActionResult Operaciones_Aritmeticas(Int32 v1=0, Int32 v2=0, int opc= 0)
         {
-            var operacion = new Operacion()
+            var operacion = new Operacion() { };
+
+            switch (opc)
             {
-                valor1 = 30,
-                valor2 = 2,
-            };
+                case 1:
+                    operacion.ope = "Suma";
+                    operacion.res = v1 + v2;
+                    break;
+                case 2:
+                    operacion.ope = "Resta";
+                    operacion.res = v1 - v2;
+                    break;
+                case 3:
+                    operacion.ope = "Multiplicación";
+                    operacion.res = v1 * v2;
+                    break;
+                case 4:
+                    operacion.ope = "División";
+                    operacion.res = v1 / v2;
+                    break;
+                default:
+                    operacion.ope = "No definida";
+                    operacion.res = 0;
+                    break;
+            } 
             return View(operacion);
         }
     }
--- ADSEvaluacion1/Models/Operacion.cs
+++ ADSEvaluacion1/Models/Operacion.cs
@@ -7,12 +7,7 @@ namespace ADSEvaluacion1.Models
 {
     public class Operacion
     {
-        public double valor1 { get; set; }
-        public double valor2 { get; set; }
-        public double suma { get; set; }
-        public double resta { get; set; }
-        public double multiplicacion { get; set; }
-        public double division { get; set; }
-
+        public String ope { get; set; }
+        public double res { get; set; }
     }
 }
